d5ae3a9153753ce04a2180a6d8428084d31111a8
uno <st9007a@gmail.com>
2016-07-27 17:20:04 +0800

    bulid framework

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = require("express")();
 var express = require("express");
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var regist_content = require('./registfile.json');
 var port = 8072;
 var nsp = io.of("/mbilab");
 
@@ -10,17 +11,18 @@ var video_queue = [];
 http.listen(port);
 
 app.use(express.static(__dirname + "/public"));
-
 nsp.on("connection", function(socket){
 	console.log("connect");
+	socket.emit("request", regist_content);
 	socket.on("request", function(data){
-		console.log("req");
+		console.log(regist_content);
+		
 	});
 	socket.on("send_youtube_id", function(data){
 		console.log(data);
 	});
 });
 
-app.get("/client", function(req, res){
+app.get("/", function(req, res){
 	res.sendfile("public/client.html");	
 });
--- public/client.js
+++ public/client.js
@@ -1,9 +1,14 @@
 var socket = io("/mbilab");
 
-socket.emit("request", "hi");
-
-
 $(function(){
+	
+	
+	socket.emit("request", "hi");
+	socket.on("request", function(data){		
+		$('.ui.search').search({source: data});
+	});
+		
+	
 
 	$("#submit").click(function(){
 		if($("#keyword").val != ""){
@@ -13,15 +18,43 @@ $(function(){
 				q : $("#keyword").val(),
 				maxResults : 10,
 			};
+			
 			var request = gapi.client.youtube.search.list(search_list_option);
 
-			//console.log(encodeURIComponent($("#keyword").val()).replace("/%20/g", "+"));
+			
 			request.execute(function(response){
 				var youtube_id_set = [];
 				for(var i=0;i<search_list_option.maxResults;i++){
 					youtube_id_set.push(response.items[i].id.videoId);
 				}
-				socket.emit("send_youtube_id", youtube_id_set);
+				
+				
+				for(var i=0;i<search_list_option.maxResults;i++){
+					
+					var youtube_iframe = document.createElement("div");
+					youtube_iframe.setAttribute("id","iframe"+i);
+					youtube_iframe.setAttribute("class","youtube_iframe");
+					$("#result_list").append(youtube_iframe);
+					
+					var player = new YT.Player("iframe"+i, {
+						videoId: youtube_id_set[i], // YouTube 影片ID
+						width: 560,               // 播放器寬度 (px)
+						height: 316,              // 播放器高度 (px)
+						events: {
+							'onStateChange': video_state_change,
+						},
+						playerVars: {
+							controls: 1,        // 在播放器顯示暫停／播放按鈕
+							showinfo: 1,        // 隱藏影片標題
+							modestbranding: 1,  // 隱藏YouTube Logo
+							fs: 0,              // 隱藏全螢幕按鈕
+							cc_load_policty: 1, // 隱藏字幕
+							iv_load_policy: 3,  // 隱藏影片註解
+							autohide: 0         // 當播放影片時隱藏影片控制列
+						},
+						
+					});
+				}
 			});
 		}
 	});	
@@ -35,4 +68,10 @@ function init(){
 
 	});
 	//console.log("init");
+}
+
+function video_state_change(e){
+	if(e.data == 1){
+		socket.emit("send_youtube_id", e.target.b.c.videoId);
+	}
 }
