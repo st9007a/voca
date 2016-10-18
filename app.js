b4676a202216dc6c8ed2b6041a3f14b8129a726e
uno <st9007a@gmail.com>
2016-10-18 14:42:29 +0800

    add keydown enter to search

--- app.js
+++ app.js
@@ -12,201 +12,209 @@ http.listen(port);
 app.use(express.static(__dirname + "/public"));
 
 jsonfile.readFile("keyword.json", function(err, obj){
-	keyword = obj;
+  keyword = obj;
 });
 
 io.on("connection", function(socket){
-	
-	jsonfile.readFile("keyword.json", function(err, obj){
-		if(err) 
-			console.log(err);
-		else
-			socket.emit("request", obj);
-	});
 
-	socket.on('disconnect', function () {
-		if(socket.user == "client"){
-			for(var i=0;i<room.length;i++){
-				if(room[i].name == socket.room){
-					room[i].users--;
-					socket.broadcast.to(socket.room).emit("someone_join", room[i]);
-					break;
-				}
-			}
-		}
-		else if(socket.user == "voca"){
-			for(var i=0;i<room.length;i++){
-				if(room[i].name == socket.room){
-					room[i].voca = false;
-					break;
-				}
-			}
-		}
+  jsonfile.readFile("keyword.json", function(err, obj){
+    if(err) 
+      console.log(err);
+    else
+      socket.emit("request", obj);
+  });
 
-		someone_leave();
-	});
+  socket.on('disconnect', function () {
+    if(socket.user == "client"){
+      for(var i=0;i<room.length;i++){
+        if(room[i].name == socket.room){
+          room[i].users--;
+          socket.broadcast.to(socket.room).emit("someone_join", room[i]);
+          break;
+        }
+      }
+    }
+    else if(socket.user == "voca"){
+      for(var i=0;i<room.length;i++){
+        if(room[i].name == socket.room){
+          room[i].voca = false;
+          break;
+        }
+      }
+    }
 
-	socket.on("keyword", function(data){
-		keyword.push({title:data});
-	});
+    someone_leave();
+  });
 
-	socket.on("voca_join", function(data){
-		
-		var is_exist = false;
-		var index = -1;
-		
-		for(var i=0;i<room.length;i++){
-			if(room[i].name == data){
-				is_exist = true;
-				index = i;
-				break;
-			}
-		}
-		
-		if(is_exist){
-			if(!room[index].voca){
-				socket.room = data;
-				socket.user = "voca";
-				socket.join(data);
-				room[index].voca = true;
-				socket.emit("voca_join", {name: room[index].name});
-			}
-			else{
-				socket.emit("voca_join", "There is a voca in the room.");
-			}
-		}
-		else{
-			socket.emit("voca_join", "The room is not exist.");
-		}
-		
-	});
+  socket.on("keyword", function(data){
+    var is_exist = false;
+    for(var i=0;i<keyword.length;i++){
+      if(keyword[i].title == data){
+        is_exist = true;
+        break;
+      }
+    }
+    if(!is_exist) keyword.push({title:data});
+  });
 
-	socket.on("voca_leave", function(data){
-		for(var i=0;i<room.length;i++){
-			if(room[i].name == data){
-				room[i].voca = false;
-			}
-		}
-		someone_leave();
-	});
-	socket.on("create_room", function(data){
-		
-		var is_exist = false;
-		
-		for(var i=0;i<room.length;i++){
-			if(room[i].name == data){
-				is_exist = true;
-				break;
-			}
-		}
-		
-		if(!is_exist){
-			socket.room = data;
-			socket.user = "client";
-			
-			socket.join(data);
-			
-			var room_data = {
-				name: data,
-				users: 1,
-				voca: false,
-			}
-			room.push(room_data);
-			
-			socket.emit("room", room_data);
-		}
-		else{
-			socket.emit("room", "The room is exist.");
-		}
-	});
-	
-	socket.on("join_room", function(data){
-		
-		var is_exist = false;
-		var index = -1;
-		
-		for(var i=0;i<room.length;i++){
-			if(room[i].name == data){
-				is_exist = true;
-				index = i;
-				break;
-			}
-		}
-		
-		if(is_exist){
-			
-			socket.room = data;
-			socket.user = "client";
-			socket.join(data);
-			
-			room[index].users++;
-			
-			socket.emit("room", room[i]);
-			socket.broadcast.to(socket.room).emit("someone_join", room[index]);
-		}
-		else{
-			socket.emit("room", "The room is not exist.");
-		}
-	});
-	
-	socket.on("leave_room", function(data){
-		for(var i=0;i<room.length;i++){
-			if(room[i].name == data){
-				room[i].users--;
-				socket.broadcast.to(socket.room).emit("someone_join", room[i]);
-				socket.leave(socket.room);
-				break;
-			}
-		}
-		someone_leave();
-		
-	});
-	
-	socket.on("send_youtube_id", function(data){
-		socket.broadcast.to(socket.room).emit("send_youtube_id", data);
-	});
-	
-	socket.on("insert_youtube_id", function(data){
-		socket.broadcast.to(socket.room).emit("insert_youtube_id", data);
-	});
-	
-	socket.on("request_video_list", function(data){
-		socket.broadcast.to(socket.room).emit("request_video_list", data);
-	});
-	
-	socket.on("control_video", function(data){
-		socket.broadcast.to(socket.room).emit("control_video", data);
-	});
-	
-	socket.on("control_volume", function(data){
-		socket.broadcast.to(socket.room).emit("control_volume", data);
-	});
-	
-	socket.on("video_list", function(data){
-		socket.broadcast.to(socket.room).emit("video_list", data);
-	});
+  socket.on("voca_join", function(data){
+
+    var is_exist = false;
+    var index = -1;
+
+    for(var i=0;i<room.length;i++){
+      if(room[i].name == data){
+        is_exist = true;
+        index = i;
+        break;
+      }
+    }
+
+    if(is_exist){
+      if(!room[index].voca){
+        socket.room = data;
+        socket.user = "voca";
+        socket.join(data);
+        room[index].voca = true;
+        socket.emit("voca_join", {name: room[index].name});
+      }
+      else{
+        socket.emit("voca_join", "There is a voca in the room.");
+      }
+    }
+    else{
+      socket.emit("voca_join", "The room is not exist.");
+    }
+  });
+
+  socket.on("voca_leave", function(data){
+    for(var i=0;i<room.length;i++){
+      if(room[i].name == data){
+        room[i].voca = false;
+      }
+    }
+    someone_leave();
+  });
+  socket.on("create_room", function(data){
+
+    var is_exist = false;
+
+    for(var i=0;i<room.length;i++){
+      if(room[i].name == data){
+        is_exist = true;
+        break;
+      }
+    }
+
+    if(!is_exist){
+      socket.room = data;
+      socket.user = "client";
+
+      socket.join(data);
+
+      var room_data = {
+        name: data,
+        users: 1,
+        voca: false,
+      }
+      room.push(room_data);
+
+      socket.emit("room", room_data);
+    }
+    else{
+      socket.emit("room", "The room is exist.");
+    }
+  });
+
+  socket.on("join_room", function(data){
+
+    var is_exist = false;
+    var index = -1;
+
+    for(var i=0;i<room.length;i++){
+      if(room[i].name == data){
+        is_exist = true;
+        index = i;
+        break;
+      }
+    }
+
+    if(is_exist){
+
+      socket.room = data;
+      socket.user = "client";
+      socket.join(data);
+
+      room[index].users++;
+
+      socket.emit("room", room[i]);
+      socket.broadcast.to(socket.room).emit("someone_join", room[index]);
+    }
+    else{
+      socket.emit("room", "The room is not exist.");
+    }
+  });
+
+  socket.on("leave_room", function(data){
+    for(var i=0;i<room.length;i++){
+      if(room[i].name == data){
+        room[i].users--;
+        socket.broadcast.to(socket.room).emit("someone_join", room[i]);
+        socket.leave(socket.room);
+        break;
+      }
+    }
+    someone_leave();
+  });
+
+  socket.on("send_youtube_id", function(data){
+    socket.broadcast.to(socket.room).emit("send_youtube_id", data);
+  });
+
+  socket.on("insert_youtube_id", function(data){
+    socket.broadcast.to(socket.room).emit("insert_youtube_id", data);
+  });
+
+  socket.on("request_video_list", function(data){
+    socket.broadcast.to(socket.room).emit("request_video_list", data);
+  });
+
+  socket.on("control_video", function(data){
+    socket.broadcast.to(socket.room).emit("control_video", data);
+  });
+
+  socket.on("control_volume", function(data){
+    socket.broadcast.to(socket.room).emit("control_volume", data);
+  });
+
+  socket.on("video_list", function(data){
+    socket.broadcast.to(socket.room).emit("video_list", data);
+  });
 });
 
 app.get("/", function(req, res){
-	res.sendfile("public/client.html");	
+  res.sendfile("public/client.html");	
 });
 app.get("/voca", function(req, res){
-	res.sendfile("public/voca.html");
+  res.sendfile("public/voca.html");
 });
 
 function someone_leave(){	
-	for(var i=0;i<room.length;i++){
-		if(room[i].users == 0 && !room[i].voca){
-			room.splice(i, 1);
-		}
-	}
+  for(var i=0;i<room.length;i++){
+    if(room[i].users == 0 && !room[i].voca){
+      room.splice(i, 1);
+    }
+  }
 }
 setInterval(function(){
-	jsonfile.writeFile("keyword.json", keyword, function(err){
-	    if(err) console.log(err);
-	})
+  jsonfile.writeFile("keyword.json", keyword, function(err){
+      if(err) console.log(err);
+  })
 }, 3000);
 app.get("/room", function(req, res){
-	res.send(room);
+  res.send(room);
 });
-//# vi:nu:wrap
+app.get("/keyword", function(req, res){
+  res.send(keyword);
+});
+//# vi:nu:wrap:sw=2:ts=2:et
--- public/js/client.js
+++ public/js/client.js
@@ -1,246 +1,243 @@
 $(function(){
-	
-	
-	var socket = io();
-	var youtube_data = [];	
-	var message_queue = [0,1,2,3,4];
-	
-	if(localStorage.getItem("room") != undefined){
-		socket.emit("join_room", localStorage.getItem("room"));
-	}
-	
-		
-	socket.on("request", function(data){		
-		$('.ui.search').search({source: data});
-	});
-		
-	socket.on("room", function(data){
-		if(data.name != undefined){
-			$("#input_room_name").val("");
-			$("#room").show();
-			$("#join_room_form").hide();
-			$("#error_msg").html("");
-			$("#room_name").html("Room : "+data.name);
-			$("#num_of_people").html("Online users : "+data.users);
-			localStorage.setItem("room", data.name);
-			socket.emit("request_video_list", true);
-		}
-		else{
-			$("#error_msg").html(data);
-		}
-	});
-	
-	socket.on("someone_join", function(data){
-		$("#num_of_people").html("Online users : "+data.users);
-	});
-	
-	$(".ui.range").range({
-		min: 0,
-		max: 10,
-		start: 10,
-		onChange: function(val){
-			socket.emit("control_volume", val*10);
-		},
-	});	
-	
-	$(".item").click(function(){
-		
-		if(localStorage.getItem("room") != undefined){
-		
-			$(".item").attr("class", "item");
-			$(this).attr("class", "item active");
-			
-			
-			switch($(this).attr("id")){
-				case "search_menu" :
-					$(".panel").hide();
-					$("#result_list").show();
-					break;
-				case "playlist_menu" : 
-					$(".panel").hide();
-					$("#play_list").show();
-					break;
-				default:
-					$(".panel").hide();
-					$("#room_list").show();
-			}
-		}
-	});
-	
-	$("#create_room").click(function(){
-		if($("#input_room_name").val() != ""){
-			socket.emit("create_room", $("#input_room_name").val());
-		}
-	});
-	
-	$("#join_room").click(function(){
-		if($("#input_room_name").val() != ""){
-			socket.emit("join_room", $("#input_room_name").val());
-		}
-	});
-	
-	$("#leave_room").click(function(){
-		socket.emit("leave_room", localStorage.getItem("room"));
-		$("#playlist_table").html("");
-		$("#title_play_now").html("目前沒有曲目");
-		window.localStorage.clear();
-		$("#room").hide();
-		$("#join_room_form").show();
-		$("#error_msg").html("");
-	});
-	
-	$("#submit").click(function(){ 
-		if($("#keyword").val() != "" && localStorage.getItem("room") != undefined){
 
-			socket.emit("keyword", $("#keyword").val());
+  var socket = io();
+  var youtube_data = [];	
+  var message_queue = [0,1,2,3,4];
 
-			$(".item").attr("class", "item");
-			$("#search_menu").attr("class", "item active");
-			
-			$(".panel").hide();
-			$("#result_list").show();
-			
-			youtube_data = [];
-			
-			var search_list_option = {
-				part : "snippet",
-				type : "video",
-				q : $("#keyword").val(),
-				maxResults : 10,
-			};
-			
-			var request = gapi.client.youtube.search.list(search_list_option);
-			
-			request.execute(function(response){
-				
-				$("#result").show();
-				$(".youtube_iframe").remove();
-								
-				for(var i=0;i<search_list_option.maxResults;i++){
-					youtube_data.push({
-						id: response.items[i].id.videoId,
-						title: response.items[i].snippet.title,
-					});
-				}
-				
-				var result_count = youtube_data.length > 10 ? 10 : youtube_data.length;
-				
-				for(var i=0;i<result_count;i++){
-					
-					$("#iframe_title"+i).html(youtube_data[i].title);
-					
-					var youtube_iframe = document.createElement("div");
-					youtube_iframe.setAttribute("id","youtube_iframe"+i);
-					youtube_iframe.setAttribute("class","youtube_iframe");
-					$("#iframe"+i).append(youtube_iframe);
-					
-					
-					var player = new YT.Player("youtube_iframe"+i, {
-						videoId: youtube_data[i].id, 
-						width: 560,              
-						height: 316,              
-						events: {
-							'onStateChange': video_state_change,
-						},
-						playerVars: {
-							controls: 1,        // 在播放器顯示暫停／播放按鈕
-							showinfo: 1,        // 隱藏影片標題
-							modestbranding: 1,  // 隱藏YouTube Logo
-							fs: 0,              // 隱藏全螢幕按鈕
-							cc_load_policty: 1, // 隱藏字幕
-							iv_load_policy: 3,  // 隱藏影片註解
-							autohide: 0,        // 當播放影片時隱藏影片控制列
-						},
-						
-					});
-				}
-			});
-		}
-	});	
-	
-	
-	$(".add").click(function(){
-		
-		var list = $(this).attr("data-list");
-		socket.emit("send_youtube_id", {
-			id : youtube_data[list].id,
-			title: youtube_data[list].title,
-		});
-		
-		if(message_queue.length != 0){ 
-			var num = message_queue.shift();
-			$(".msg"+num).html("加入播放清單");
-			$(".message.content"+num).html("將"+youtube_data[list].title+"加入播放清單");
-			$("#msg_box"+num).transition("fade up");
-			setTimeout(function(){
-				$("#msg_box"+num).transition("fade up");
-				message_queue.push(num);
-			}, 2000);
-			
-			
-		}
-	});
-	
-	$(".insert").click(function(){
-		var list = $(this).attr("data-list");
-		socket.emit("insert_youtube_id", {
-			id : youtube_data[list].id,
-			title: youtube_data[list].title,
-		});
-		
-		if(message_queue.length != 0){
-			var num = message_queue.shift();
-			$(".header.msg"+num).html("插播為下一首");
-			$(".message.content"+num).html("將"+youtube_data[list].title+"插播為下一首");
-			$("#msg_box"+num).transition("fade up");
-			setTimeout(function(){
-				$("#msg_box"+num).transition("fade up");
-				message_queue.push(num);
-			}, 2000);
-		}
-	});
-	
-	$("#play_video").click(function(){
-		socket.emit("control_video", 0);
-	});
-	$("#pause_video").click(function(){
-		socket.emit("control_video", 1);
-	});
-	$("#next_video").click(function(){
-		socket.emit("control_video", 2);
-	});
-	
-	socket.on("video_list", function(data){
-		
-		var play_now_description = (data.now.title != undefined) ? "現在播放 : "+ data.now.title.toString() : "目前沒有曲目";
-		$("#title_play_now").html(play_now_description);
-		
-		if(data.list != -1){
-			$("#playlist_table").transition("fade up");
-			$("#playlist_table").html("");
-			for(var i=0;i<data.list.length;i++){
-				$("#playlist_table").append("<tr><td>"+(i+1)+"</td><td>"+data.list[i].title+"</td><tr>");
-			}
-			$("#playlist_table").transition("fade up");
-		}
-		
-	});
-	
-	
-	$(document).scroll(function(){     
-		if($(document).height() == ($(window).height() + $(this).scrollTop())){   
-			
-		}  
-	});
-	
-	function video_state_change(e){
-		// if(e.data == 1){
-			// socket.emit("send_youtube_id", e.target.b.c.videoId);
-			// for(var i=0;i<10;i++){
-				// $("#iframe"+i).remove();
-			// }
-		// }
-	}
+  if(localStorage.getItem("room") != undefined){
+    socket.emit("join_room", localStorage.getItem("room"));
+  }
+
+  socket.on("request", function(data){
+    $('.ui.search').search({source: data});
+  });
+
+  socket.on("room", function(data){
+    if(data.name != undefined){
+      $("#input_room_name").val("");
+      $("#room").show();
+      $("#join_room_form").hide();
+      $("#error_msg").html("");
+      $("#room_name").html("Room : "+data.name);
+      $("#num_of_people").html("Online users : "+data.users);
+      localStorage.setItem("room", data.name);
+      socket.emit("request_video_list", true);
+    }
+    else{
+      $("#error_msg").html(data);
+    }
+  });
+
+  socket.on("someone_join", function(data){
+    $("#num_of_people").html("Online users : "+data.users);
+  });
+
+  $(".ui.range").range({
+    min: 0,
+    max: 10,
+    start: 10,
+    onChange: function(val){
+      socket.emit("control_volume", val*10);
+    },
+  });
+
+  $(".item").click(function(){
+
+    if(localStorage.getItem("room") != undefined){
+
+      $(".item").attr("class", "item");
+      $(this).attr("class", "item active");
+
+      switch($(this).attr("id")){
+        case "search_menu" :
+          $(".panel").hide();
+          $("#result_list").show();
+          break;
+        case "playlist_menu" :
+          $(".panel").hide();
+          $("#play_list").show();
+          break;
+        default:
+          $(".panel").hide();
+          $("#room_list").show();
+      }
+  }
+  });
+
+  $("#create_room").click(function(){
+    if($("#input_room_name").val() != ""){
+      socket.emit("create_room", $("#input_room_name").val());
+    }
+  });
+
+  $("#join_room").click(function(){
+    if($("#input_room_name").val() != ""){
+      socket.emit("join_room", $("#input_room_name").val());
+    }
+  });
+
+  $("#leave_room").click(function(){
+    socket.emit("leave_room", localStorage.getItem("room"));
+    $("#playlist_table").html("");
+    $("#title_play_now").html("目前沒有曲目");
+    window.localStorage.clear();
+    $("#room").hide();
+    $("#join_room_form").show();
+    $("#error_msg").html("");
+  });
+  function search(){ 
+    if($("#keyword").val() != "" && localStorage.getItem("room") != undefined){
+
+      socket.emit("keyword", $("#keyword").val());
+
+      $(".item").attr("class", "item");
+      $("#search_menu").attr("class", "item active");
+
+      $(".panel").hide();
+      $("#result_list").show();
+
+      youtube_data = [];
+
+      var search_list_option = {
+        part : "snippet",
+        type : "video",
+        q : $("#keyword").val(),
+        maxResults : 10,
+      };
+
+      var request = gapi.client.youtube.search.list(search_list_option);
+
+      request.execute(function(response){
+
+        $("#result").show();
+        $(".youtube_iframe").remove();
+
+        for(var i=0;i<search_list_option.maxResults;i++){
+          youtube_data.push({
+            id: response.items[i].id.videoId,
+            title: response.items[i].snippet.title,
+          });
+        }
+
+        var result_count = youtube_data.length > 10 ? 10 : youtube_data.length;
+
+        for(var i=0;i<result_count;i++){
+
+          $("#iframe_title"+i).html(youtube_data[i].title);
+
+          var youtube_iframe = document.createElement("div");
+          youtube_iframe.setAttribute("id","youtube_iframe"+i);
+          youtube_iframe.setAttribute("class","youtube_iframe");
+          $("#iframe"+i).append(youtube_iframe);
+
+
+          var player = new YT.Player("youtube_iframe"+i, {
+            videoId: youtube_data[i].id,
+            width: 560,
+            height: 316,
+            events: {
+              'onStateChange': video_state_change,
+            },
+            playerVars: {
+              controls: 1,        // 在播放器顯示暫停／播放按鈕
+              showinfo: 1,        // 隱藏影片標題
+              modestbranding: 1,  // 隱藏YouTube Logo
+              fs: 0,              // 隱藏全螢幕按鈕
+              cc_load_policty: 1, // 隱藏字幕
+              iv_load_policy: 3,  // 隱藏影片註解
+              autohide: 0,        // 當播放影片時隱藏影片控制列
+            },
+
+          });
+        }
+      });
+    }
+  }
+  $("#submit").click(search());
+  document.addEventListener("keydown", function(e){
+    if(e.which == 13) search()
+  });
+
+  $(".add").click(function(){
+
+    var list = $(this).attr("data-list");
+    socket.emit("send_youtube_id", {
+      id : youtube_data[list].id,
+      title: youtube_data[list].title,
+    });
+
+    if(message_queue.length != 0){ 
+      var num = message_queue.shift();
+      $(".msg"+num).html("加入播放清單");
+      $(".message.content"+num).html("將"+youtube_data[list].title+"加入播放清單");
+      $("#msg_box"+num).transition("fade up");
+      setTimeout(function(){
+        $("#msg_box"+num).transition("fade up");
+        message_queue.push(num);
+      }, 2000);
+    }
+  });
+
+  $(".insert").click(function(){
+    var list = $(this).attr("data-list");
+    socket.emit("insert_youtube_id", {
+      id : youtube_data[list].id,
+      title: youtube_data[list].title,
+  });
+
+    if(message_queue.length != 0){
+      var num = message_queue.shift();
+      $(".header.msg"+num).html("插播為下一首");
+      $(".message.content"+num).html("將"+youtube_data[list].title+"插播為下一首");
+      $("#msg_box"+num).transition("fade up");
+      setTimeout(function(){
+        $("#msg_box"+num).transition("fade up");
+        message_queue.push(num);
+      }, 2000);
+    }
+  });
+
+  $("#play_video").click(function(){
+    socket.emit("control_video", 0);
+  });
+  $("#pause_video").click(function(){
+    socket.emit("control_video", 1);
+  });
+  $("#next_video").click(function(){
+    socket.emit("control_video", 2);
+  });
+
+  socket.on("video_list", function(data){
+
+    var play_now_description = (data.now.title != undefined) ? "現在播放 : "+ data.now.title.toString() : "目前沒有曲目";
+    $("#title_play_now").html(play_now_description);
+
+    if(data.list != -1){
+      $("#playlist_table").transition("fade up");
+      $("#playlist_table").html("");
+      for(var i=0;i<data.list.length;i++){
+        $("#playlist_table").append("<tr><td>"+(i+1)+"</td><td>"+data.list[i].title+"</td><tr>");
+      }
+      $("#playlist_table").transition("fade up");
+    }
+
+  });
+
+
+  $(document).scroll(function(){
+    if($(document).height() == ($(window).height() + $(this).scrollTop())){
+
+    }
+  });
+
+  function video_state_change(e){
+    // if(e.data == 1){
+      // socket.emit("send_youtube_id", e.target.b.c.videoId);
+      // for(var i=0;i<10;i++){
+        // $("#iframe"+i).remove();
+      // }
+    // }
+  }
 })
 
 function init(){
@@ -251,4 +248,4 @@ function init(){
 	});
 	//console.log("init");
 }
-// # vi:nu:wrap
+// # vi:nu:ts=2:sw=2:et
